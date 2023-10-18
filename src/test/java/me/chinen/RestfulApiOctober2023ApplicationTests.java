8f5f2f5d619951dff19ae5d65b43b9be4f16fd06
RodrigoChinen <eng.chinen@gmail.com>
2023-10-18 11:29:56 -0300

    first commit

---
+++ src/test/java/me/chinen/RestfulApiOctober2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chinen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiOctober2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
